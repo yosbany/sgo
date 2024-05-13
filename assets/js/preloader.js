ac2a1c63a50359ff2cfca1131b924fc2cdfb7efb
yosbany <ytejas.86@gmail.com>
2024-05-13 15:03:22 -0300

    ok

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -1,7 +1,22 @@
-var pre = document.createElement("div");
-pre.innerHTML = '<div class="loader-wrapper"><div class="loader">Loading...</div></div>';
-document.body.insertBefore(pre, document.body.firstChild);
+// Función para mostrar el loader
+function showLoader() {
+    var pre = document.createElement("div");
+    pre.innerHTML = '<div class="loader-wrapper"><div class="loader">Loading...</div></div>';
+    document.body.insertBefore(pre, document.body.firstChild);
+}
 
+// Función para marcar la página como cargada
+function markPageLoaded() {
+    document.body.className += " loaded";
+}
+
+// Mostrar el loader cuando se carga la página
 document.addEventListener("DOMContentLoaded", function(event) {
-    document.body.className += " loaded"
-}); 
+    showLoader();
+});
+
+// Marcar la página como cargada cuando cambia el hash de la URL
+window.addEventListener("hashchange", function() {
+    showLoader();
+    markPageLoaded();
+});
