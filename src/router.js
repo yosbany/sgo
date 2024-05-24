83ba1a7204d79674f2d90b3642028110f665d07a
yosbany <ytejas.86@gmail.com>
2024-05-24 12:29:24 -0300

    ok

--- src/router.js
+++ src/router.js
@@ -58,7 +58,7 @@ function isRoutePublic(key) {
 
 function getKeyFromHashAndPath() {
     const hash = window.location.hash.slice(1).split('?')[0];
-    const path = window.location.pathname.slice(BASE_PATH.length).split('?')[0];
+    let path = window.location.pathname.slice(BASE_PATH.length).split('?')[0];
     const dotIndex = path ? path.indexOf('.') : -1;
     if (dotIndex !== -1) {
         path = path.slice(0, dotIndex);
