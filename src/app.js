18645ae385eff14e06c26f724a7eb1e806a24bba
yosbany <ytejas.86@gmail.com>
2024-05-26 20:33:48 -0300

    ok

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-import router from './router.js';
-
 window.STORAGE_TYPE = 'firebase';
 //window.STORAGE_TYPE = 'localstorage';
+import router from './router.js';
+
 window.addEventListener('load', router);
 window.addEventListener('hashchange', router);
