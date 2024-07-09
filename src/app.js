13544a23ac9b8e268cc30792d40b7f71aa7ff3ef
yosbany <ytejas.86@gmail.com>
2024-07-09 09:37:51 -0300

    ok

--- src/app.js
+++ src/app.js
@@ -3,19 +3,3 @@ import router from './router.js';
 
 window.addEventListener('load', router);
 window.addEventListener('hashchange', router);
-
-var timestamp = new Date().getTime();
-
-// Obtener todos los elementos link que cargan CSS
-var links = document.querySelectorAll('link[rel="stylesheet"]');
-links.forEach(function(link) {
-    var href = link.getAttribute('href');
-    link.setAttribute('href', href + (href.includes('?') ? '&' : '?') + 'v=' + timestamp);
-});
-
-// Obtener todos los elementos script que cargan JS
-var scripts = document.querySelectorAll('script[src]');
-scripts.forEach(function(script) {
-    var src = script.getAttribute('src');
-    script.setAttribute('src', src + (src.includes('?') ? '&' : '?') + 'v=' + timestamp);
-});
